69a33647f04e1fdbfc0a93488988c7394dfa2475
skylocke <mathematical.fiction@gmail.com>
2017-04-17 14:28:46 -0700

    tweaks

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 (function() {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
+  var canvas = document.getElementById("canvas"),
+      context = canvas.getContext("2d");
 
-  function rect(x, y, w, h) {
+  var blue = "#5A9BD5",
+      yellow = "#FFFF01",
+      unit = 60,
+      blueFactor = 5,
+      yellowFactor = blueFactor/2;
+
+  function rectangle(ctx, x, y, w, h, color) {
+    context = ctx;
+    context.fillStyle = color;
     context.beginPath();
     context.rect(x,y,w,h);
     context.closePath();
     context.fill();
   }
+
+  rectangle(context, 0, 0, blueFactor*unit, blueFactor*unit, blue);
+  rectangle(context, yellowFactor/2*unit, yellowFactor/2*unit, yellowFactor*unit, yellowFactor*unit, yellow);
 
 })();
